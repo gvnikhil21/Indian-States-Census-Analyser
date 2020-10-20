1afa4ac50c1216440589d4e9aec27f623d4fa249
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-10-20 21:32:23 +0530

    Added: Test when given correct file but inconsistent data type throws custom censusAnalyserException of specific type functionality added

--- src/test/java/com/bridgelabs/censusanalyser/tester/CensusAnalyserTest.java
+++ src/test/java/com/bridgelabs/censusanalyser/tester/CensusAnalyserTest.java
@@ -13,6 +13,7 @@ public class CensusAnalyserTest {
 	private static final Logger LOGGER = LogManager.getLogger(CensusAnalyserTest.class);
 	private static final String INDIA_CENSUS_CSV_FILE_PATH = "D:\\AssignmentBridgeLabs\\Indian-States-Census-Analyser\\src\\main\\resources\\IndiaStateCensusData.csv";
 	private static final String WRONG_CSV_FILE_PATH = "D:\\AssignmentBridgeLabs\\Indian-States-Census-Analyser\\src\\main\\resources\\IndiaStateCensusDataOne.csv";
+	private static final String INDIAN_STATECODES_CSVFILE = "D:\\AssignmentBridgeLabs\\Indian-States-Census-Analyser\\src\\main\\resources\\IndiaStateCode.csv";
 
 	@Test
 	public void givenIndianCensusCSVFile_ShouldReturnCorrectEntries() {
@@ -22,7 +23,6 @@ public class CensusAnalyserTest {
 			Assert.assertEquals(29, noOfEntries);
 			LOGGER.info("Test performed for no. of entries in given CSV file");
 		} catch (Exception e) {
-			e.getMessage();
 		}
 	}
 
@@ -36,6 +36,17 @@ public class CensusAnalyserTest {
 		} catch (CensusAnalyserException e) {
 			Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
 			LOGGER.info("Test performed for given wrong CSV file");
+		}
+	}
+
+	@Test
+	public void GivenTheStateCensusCsvFile_WhenCorrect_ButFileExtensionIncorrect_ShouldThrowCensusAnalyserException() {
+		try {
+			CensusAnalyserMain censusAnalyserMain = new CensusAnalyserMain();
+			censusAnalyserMain.loadIndiaCensusData(INDIAN_STATECODES_CSVFILE);
+		} catch (CensusAnalyserException e) {
+			Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_TYPE_PROBLEM, e.type);
+			LOGGER.info("Test performed for given correct CSV file but inconsistent type data");
 		}
 	}
 }
