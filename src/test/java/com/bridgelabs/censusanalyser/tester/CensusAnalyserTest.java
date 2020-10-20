dc262f6acc37c2f44f1f119683a62081b7c443a8
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-10-20 21:05:50 +0530

    Added: Test when given wrong file throws custom censusAnalyserException functionality added

--- src/test/java/com/bridgelabs/censusanalyser/tester/CensusAnalyserTest.java
+++ src/test/java/com/bridgelabs/censusanalyser/tester/CensusAnalyserTest.java
@@ -4,12 +4,15 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import com.bridgelabs.censusanalyser.controller.CensusAnalyserMain;
+import com.bridgelabs.censusanalyser.model.CensusAnalyserException;
 
 public class CensusAnalyserTest {
 	private static final Logger LOGGER = LogManager.getLogger(CensusAnalyserTest.class);
 	private static final String INDIA_CENSUS_CSV_FILE_PATH = "D:\\AssignmentBridgeLabs\\Indian-States-Census-Analyser\\src\\main\\resources\\IndiaStateCensusData.csv";
+	private static final String WRONG_CSV_FILE_PATH = "D:\\AssignmentBridgeLabs\\Indian-States-Census-Analyser\\src\\main\\resources\\IndiaStateCensusDataOne.csv";
 
 	@Test
 	public void givenIndianCensusCSVFile_ShouldReturnCorrectEntries() {
@@ -23,4 +26,16 @@ public class CensusAnalyserTest {
 		}
 	}
 
+	@Test
+	public void givenIndiaCensusData_WithWrongFile_ShouldThrowException() {
+		try {
+			CensusAnalyserMain censusAnalyserMain = new CensusAnalyserMain();
+			ExpectedException exceptionRule = ExpectedException.none();
+			exceptionRule.expect(CensusAnalyserException.class);
+			censusAnalyserMain.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+		} catch (CensusAnalyserException e) {
+			Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+			LOGGER.info("Test performed for given wrong CSV file");
+		}
+	}
 }
