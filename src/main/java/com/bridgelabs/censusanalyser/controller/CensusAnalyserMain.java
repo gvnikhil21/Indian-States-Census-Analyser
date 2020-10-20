ab87b2aab74bc338601d377251c795aa55fdbc4c
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-10-20 20:49:25 +0530

    Added: Load data from the given CSV file functionality added

--- src/main/java/com/bridgelabs/censusanalyser/controller/CensusAnalyserMain.java
+++ src/main/java/com/bridgelabs/censusanalyser/controller/CensusAnalyserMain.java
@@ -1,7 +1,15 @@
 package com.bridgelabs.censusanalyser.controller;
 
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import com.bridgelabs.censusanalyser.model.*;
 
 public class CensusAnalyserMain {
 
@@ -10,5 +18,29 @@ public class CensusAnalyserMain {
 	public static void main(String[] args) {
 		// welcome message
 		LOGGER.info("Welcome to Indian States Census Analyser!");
+
+		CensusAnalyserMain censusAnalyserMain = new CensusAnalyserMain();
+		censusAnalyserMain.loadIndiaCensusData(
+				"D:\\AssignmentBridgeLabs\\Indian-States-Census-Analyser\\src\\main\\resources\\IndiaStateCensusData.csv");
+	}
+
+	public int loadIndiaCensusData(String csvFilePath) {
+		int noOfEntries = 0;
+		try {
+			Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+			CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+			csvToBeanBuilder.withType(IndiaCensusCSV.class);
+			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+			CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
+			Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();
+			while (censusCSVIterator.hasNext()) {
+				noOfEntries++;
+				censusCSVIterator.next();
+			}
+			LOGGER.info("No. of entries in the file : " + noOfEntries);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return noOfEntries;
 	}
 }
