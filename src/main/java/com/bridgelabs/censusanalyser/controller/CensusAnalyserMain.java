023e8721fcb092a5e91d77c2b7de798f4883f3fd
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-10-20 20:37:16 +0530

    Added: Welcome message added to test project config

---
+++ src/main/java/com/bridgelabs/censusanalyser/controller/CensusAnalyserMain.java
@@ -0,0 +1,14 @@
+package com.bridgelabs.censusanalyser.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class CensusAnalyserMain {
+
+	private static final Logger LOGGER = LogManager.getLogger(CensusAnalyserMain.class);
+
+	public static void main(String[] args) {
+		// welcome message
+		LOGGER.info("Welcome to Indian States Census Analyser!");
+	}
+}
