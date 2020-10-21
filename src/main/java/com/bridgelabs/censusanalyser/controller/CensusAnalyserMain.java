210c72ac2f7e73beb08646c22095a92f8bc175cf
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-10-21 11:31:06 +0530

    Added: Handle exception when given wrong file path functionality added

--- src/main/java/com/bridgelabs/censusanalyser/controller/CensusAnalyserMain.java
+++ src/main/java/com/bridgelabs/censusanalyser/controller/CensusAnalyserMain.java
@@ -31,11 +31,11 @@ public class CensusAnalyserMain {
 				if (noOfEntries == 0) {
 					String checkArray[] = line.trim().split(",");
 					if (checkArray.length != 4)
-						throw new CensusAnalyserException("Wrong Delimiter",
+						throw new CensusAnalyserException("Wrong header",
 								CensusAnalyserException.ExceptionType.HEADER_PROBLEM);
 					if (!checkArray[0].equals("State") || !checkArray[1].equals("Population")
 							|| !checkArray[2].equals("AreaInSqKm") || !checkArray[3].equals("DensityPerSqKm"))
-						throw new CensusAnalyserException("Wrong Delimiter",
+						throw new CensusAnalyserException("Wrong class type",
 								CensusAnalyserException.ExceptionType.CENSUS_TYPE_PROBLEM);
 				}
 				noOfEntries++;
@@ -56,6 +56,9 @@ public class CensusAnalyserMain {
 			while ((line = reader.readLine()) != null) {
 				noOfEntries++;
 			}
+		} catch (FileNotFoundException e) {
+			throw new CensusAnalyserException("File not exists",
+					CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
